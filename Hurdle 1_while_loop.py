92d3d059b85e67b6003511864f6ff8dadc38a62d
farfuad <86918305+farfuad@users.noreply.github.com>
2021-07-10 20:23:50 +0600

    Add files via upload

---
+++ Hurdle 1_while_loop.py
@@ -0,0 +1,32 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+hurdles = 6
+while hurdles > 0:
+    move()
+    jump()
+    hurdles -= 1
+
+    
+
+    
+    
+    
+
+    
+        
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
